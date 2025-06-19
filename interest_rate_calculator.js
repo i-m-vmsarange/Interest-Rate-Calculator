5bbc2994be7e397d247e8c10546ea4df9c31aec0
VAISHNAVI SARANGE <sarangevaishnavi55@gmail.com>
2025-06-19 15:38:45 +0530

    Added functionality to interest-calculator

---
+++ interest_rate_calculator.js
@@ -0,0 +1,23 @@
+// Formula : A = P(1+r/n)^nt + PMT((1+r/n)^nt - 1)/(r/n)
+
+const btn = document.getElementById('calculate-button');
+btn.addEventListener('click',calculate);
+
+function calculate(){
+     
+    let P = Number(document.getElementById("starting-balance").value);
+    let PMT = Number(document.getElementById("contribution").value);
+    let t = Number(document.getElementById("years").value);
+    let r = Number(document.getElementById("interest-rate").value)/100;
+    let n = Number(document.getElementById("compound").value);
+
+     let temp = Math.pow((1+r/n),n*t);
+
+     let result1 = P*temp;
+     let result2 = (PMT*(temp-1))/(r/n);
+
+     let result = result1 + result2;
+
+     document.getElementById("result").innerHTML = result.toFixed(2);
+    
+}
